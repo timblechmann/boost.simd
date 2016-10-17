a85a10ab058d2e56cc5c59df8244bfc40e17fa57
jtlap <jtlapreste@gmail.com>
2016-10-17 12:40:48 +0200

    splitting the tests for cast

--- test/function/simd/cast/cast_test_base.hpp
+++ test/function/simd/cast/cast_test_base.hpp
@@ -7,39 +7,11 @@
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
 */
 //==================================================================================================
-#include <boost/simd/function/cast.hpp>
-#include <boost/simd/pack.hpp>
-#include <simd_test.hpp>
+#ifndef CAST_TEST_BASE_HPP
+#define CAST_TEST_BASE_HPP
 #include <boost/simd/function/enumerate.hpp>
 
 namespace bs = boost::simd;
-using bs::pack;
-
-// template <typename T, typename Env>
-// void test_type(Env& $)
-// {
-//   static const std::size_t N = T::static_size;
-//   STF_EXPR_IS(bs::cast<std::int8_t  >(T()), (bs::pack<std::int8_t,   N>));
-//   STF_EXPR_IS(bs::cast<std::int16_t >(T()), (bs::pack<std::int16_t,  N>));
-//   STF_EXPR_IS(bs::cast<std::int32_t >(T()), (bs::pack<std::int32_t,  N>));
-//   STF_EXPR_IS(bs::cast<std::int64_t >(T()), (bs::pack<std::int64_t,  N>));
-//   STF_EXPR_IS(bs::cast<std::uint8_t >(T()), (bs::pack<std::uint8_t,  N>));
-//   STF_EXPR_IS(bs::cast<std::uint16_t>(T()), (bs::pack<std::uint16_t, N>));
-//   STF_EXPR_IS(bs::cast<std::uint32_t>(T()), (bs::pack<std::uint32_t, N>));
-//   STF_EXPR_IS(bs::cast<std::uint64_t>(T()), (bs::pack<std::uint64_t, N>));
-//   STF_EXPR_IS(bs::cast<float        >(T()), (bs::pack<float        , N>));
-//   STF_EXPR_IS(bs::cast<double       >(T()), (bs::pack<double       , N>));
-// }
-
-
-// STF_CASE_TPL( "Check cast return type",  (int64_t))//STF_NUMERIC_TYPES )
-// {
-//   static const std::size_t N = bs::pack<T>::static_size;
-
-//   test_type<bs::pack<T, N>>($);
-//   test_type<bs::pack<T, N/2>>($);
-//   test_type<bs::pack<T, N*2>>($);
-// }
 
 template < class T, class R > struct res
 {
@@ -86,12 +58,4 @@ void test_val(Env& $)
 }
 
 
-STF_CASE_TPL( "Check cast behavior",  STF_NUMERIC_TYPES )
-{
-  static const std::size_t N = bs::pack<T>::static_size;
-
-  test_val<bs::pack<T, N>>($);
-//  test_val<bs::pack<T, N/2>>($);
-//  test_val<bs::pack<T, N*2>>($);
-
-}
+#endif
