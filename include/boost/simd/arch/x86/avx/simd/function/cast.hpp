be0be9455b17f9f24aa65c1ec3a96b0d7b01c0f3
jtlap <jtlapreste@gmail.com>
2016-10-18 14:42:05 +0200

    using 128<->256 and 265<->128 avx cvt

--- include/boost/simd/arch/x86/avx/simd/function/cast.hpp
+++ include/boost/simd/arch/x86/avx/simd/function/cast.hpp
@@ -47,6 +47,69 @@ namespace boost { namespace simd { namespace ext
     }
   };
 
+  //------------------------------------------------------------------------------------------------
+  // bitwise cvt int32<->double
+  BOOST_DISPATCH_OVERLOAD ( cast_
+                          , (typename A0, typename A1)
+                          , bs::avx_
+                          , bs::pack_<bd::int32_<A0>,bs::sse_>
+                          , bd::target_<bd::scalar_<bd::single_<A1>>>
+                          )
+  {
+    using result = typename A0::template rebind<typename A1::type>;
+    BOOST_FORCEINLINE result operator()(A0 const& a0, A1 const&) const BOOST_NOEXCEPT
+    {
+      return _mm256_cvtepi32_pd(a0);
+    }
+  };
+
+  //------------------------------------------------------------------------------------------------
+  // bitwise cvt double->int32
+  BOOST_DISPATCH_OVERLOAD ( cast_
+                          , (typename A0, typename A1)
+                          , bs::sse_
+                          , bs::pack_<bd::double_<A0>,bs::avx_>
+                          , bd::target_<bd::scalar_<bd::int32_<A1>>>
+                          )
+  {
+    using result = typename A0::template rebind<typename A1::type>;
+    BOOST_FORCEINLINE result operator()(A0 const& a0, A1 const&) const BOOST_NOEXCEPT
+    {
+      return _mm256_cvttpd_epi32(a0);
+    }
+  };
+
+  //------------------------------------------------------------------------------------------------
+  // bitwise cvt double->float
+  BOOST_DISPATCH_OVERLOAD ( cast_
+                          , (typename A0, typename A1)
+                          , bs::sse_
+                          , bs::pack_<bd::double_<A0>,bs::avx_>
+                          , bd::target_<bd::scalar_<bd::single_<A1>>>
+                          )
+  {
+    using result = typename A0::template rebind<typename A1::type>;
+    BOOST_FORCEINLINE result operator()(A0 const& a0, A1 const&) const BOOST_NOEXCEPT
+    {
+      return _mm256_cvttpd_ps(a0);
+    }
+  };
+
+  //------------------------------------------------------------------------------------------------
+  // bitwise cvt float->double
+  BOOST_DISPATCH_OVERLOAD ( cast_
+                          , (typename A0, typename A1)
+                          , bs::avx_
+                          , bs::pack_<bd::single_<A0>,bs::sse_>
+                          , bd::target_<bd::scalar_<bd::double_<A1>>>
+                          )
+  {
+    using result = typename A0::template rebind<typename A1::type>;
+    BOOST_FORCEINLINE result operator()(A0 const& a0, A1 const&) const BOOST_NOEXCEPT
+    {
+      return _mm256_cvttps_pd(a0);
+    }
+  };
 
 } } }
 
